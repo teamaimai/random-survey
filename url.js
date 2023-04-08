bc119d647e59aecd0317c2b8d0d2136132b1c719
teamaimai <105287512+teamaimai@users.noreply.github.com>
2023-04-08 16:21:03 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
 	'https://tassel.syd1.qualtrics.com/jfe/form/SV_5vCRSKkCQoodS1o',
 	'https://tassel.syd1.qualtrics.com/jfe/form/SV_emM7PzCQfI1w8IK',
 	'https://tassel.syd1.qualtrics.com/jfe/form/SV_0ewnGPltqoEl31A',

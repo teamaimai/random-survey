c5087f7205585ac92f0c31ca2f251d6855a71cc0
teamaimai <105287512+teamaimai@users.noreply.github.com>
2023-04-15 01:22:09 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,4 +10,6 @@
 const urls = [
 	'https://tassel.syd1.qualtrics.com/jfe/form/SV_5vCRSKkCQoodS1o',
 	'https://tassel.syd1.qualtrics.com/jfe/form/SV_emM7PzCQfI1w8IK',
+	'https://tassel.syd1.qualtrics.com/jfe/form/SV_5vCRSKkCQoodS1o',
+	'https://tassel.syd1.qualtrics.com/jfe/form/SV_5vCRSKkCQoodS1o',
 ];
